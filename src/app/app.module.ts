3650515677100d09d01df1fc9953693153026a84
hamzewi <hamza.missaoui47@gmail.com>
2021-10-30 11:04:35 +0100

    change routing commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+// si on veut integrer routing module f west el app module
+// import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // import { AccueilBiblioComponent } from './components/admin-section/admin-section.component';
@@ -11,7 +12,9 @@ import { AdminSectionComponent } from './components/admin-section/admin-section.
 import { BooksComponent } from './components/books/books.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+// import { AppRouting2Module } from './app-routing-2.module';
 
+// const routes: Routes = [];
 @NgModule({
   declarations: [
     // enregistrement de component (CLI)
@@ -25,6 +28,11 @@ import { RegisterComponent } from './components/register/register.component';
     RegisterComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
+  // imports: [
+  //   BrowserModule,
+  //   //  AppRoutingModule,
+  //     RouterModule.forRoot(routes),
+  //   ],
   providers: [],
   bootstrap: [AppComponent],
 })
