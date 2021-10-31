c57be77fe70defd3eb67f06757fa3026cf68f6f1
hamzewi <hamza.missaoui47@gmail.com>
2021-10-31 13:29:26 +0100

    use fontawesome icons

--- src/app/components/header-section/header-section.component.ts
+++ src/app/components/header-section/header-section.component.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit, VERSION } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebook,
+  faYoutube,
+  faTwitter,
+  faTwitch,
+} from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-header-section',
   templateUrl: './header-section.component.html',
   styleUrls: ['./header-section.component.css'],
 })
 export class HeaderSectionComponent {
-  // constructor() {}
+  searchIcon = faSearch;
+  facebook = faFacebook;
+  youtube = faYoutube;
+  twitch = faTwitch;
+  twitter = faTwitter;
+
+  constructor() {}
   // ngOnInit(): void {}
 }
